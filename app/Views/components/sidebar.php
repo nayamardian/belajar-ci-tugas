5d0b20bd00e9ebbad3e75c029fd851f44ae826b1
nayamardian <nayyamardian@gmail.com>
2025-06-18 19:55:26 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -26,6 +26,13 @@
             </li><!-- End Produk Nav -->
 
             <li class="nav-item">
+                <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+                    <i class="bi bi-person"></i>
+                    <span>Profile</span>
+                </a>
+            </li><!-- End Profile Nav -->
+
+            <li class="nav-item">
                 <a class="nav-link <?php echo (uri_string() == 'kategori') ? "" : "collapsed" ?>" href="kategori">
                     <i class="bi bi-receipt"></i>
                     <span>Kategori Produk</span>
